52c2891ef70aeafcafa1d7a4f3c0c8195e1205da
what-is-me <2454787428@qq.com>
2022-12-19 11:06:36 +0800

    完成

--- src/main/java/org/whatisme/studentqa/controller/LogSignServlet.java
+++ src/main/java/org/whatisme/studentqa/controller/LogSignServlet.java
@@ -20,11 +20,11 @@ public class LogSignServlet extends UTF8Servlet {
     @Override//登录
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
-        User nuser = User.builder()
-                .uid(Long.valueOf(req.getParameter("uid")))
-                .password(req.getParameter("password"))
-                .build();
         try {
+            User nuser = User.builder()
+                    .uid(Long.valueOf(req.getParameter("uid")))
+                    .password(req.getParameter("password"))
+                    .build();
             User user = userMapper.selectById(nuser.getUid());
             log.info(user.toString());
             if (user.getPassword().equals(nuser.getPassword())) {
@@ -42,13 +42,13 @@ public class LogSignServlet extends UTF8Servlet {
     @Override//注册
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
-        User user = User.builder()
-                .uid(Long.valueOf(req.getParameter("uid")))
-                .name(req.getParameter("name"))
-                .password(req.getParameter("password"))
-                .type("student")
-                .build();
         try {
+            User user = User.builder()
+                    .uid(Long.valueOf(req.getParameter("uid")))
+                    .name(req.getParameter("name"))
+                    .password(req.getParameter("password"))
+                    .type("student")
+                    .build();
             userMapper.insert(user);
             req.getSession().setAttribute("user", user);
             resp.getWriter().println(HttpResult.successResult);
